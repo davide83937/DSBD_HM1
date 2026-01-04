8fb812966e7a0a6f08b5bf938fc4e0d3901e4619
david <davidepanto@gmail.com>
2026-01-04 13:28:39 +0100

    migliorie

--- DataCollectorMicroservice/DataCollectorMicroservice.py
+++ DataCollectorMicroservice/DataCollectorMicroservice.py
@@ -15,6 +15,9 @@ ARRIVAL = "arrival"
 DEPARTURE = "departure"
 
 
+@app.route("/", methods=["GET"])
+def health_check():
+    return {"status": "OK"}, 200
 
 @app.route("/check_time_update", methods=["POST"])
 def check_time_update():
